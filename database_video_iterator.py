2e650f8034d499573d653a8a124e74c5d9bd431a
vishnubanna <banna3vishnu@gmail.com>
2020-06-16 13:26:53 -0700

    video iterator

--- database_video_iterator.py
+++ database_video_iterator.py
@@ -150,9 +150,9 @@ class database_video_iterator():
                     break
             del image
         
-    def get_all_frames(self, image, url = True, folder_name = None):
+    def get_all_frames(self, image, url = True, folder_name = None, filepath = "None", iter_space = 1):
         # name="http://vision.cs.luc.edu/~cv/videos/bcfe3110b5/2020-05-11_23_54_52.mp4"
-        print(folder_name)
+        print(image)
         if image == None:
             return None
         if folder_name != None:
@@ -162,92 +162,22 @@ class database_video_iterator():
         #for i, frame in enumerate(image):
         #frames = list(image)
         #del image
-        vidcap = cv2.VideoCapture(image)
-        success, frame = vidcap.read()
+        cap = cv2.VideoCapture(image)
         count = 0
-        while(success):
-             cv2.imwrite("frame%d.png" % count, frame)
-             success, image = vidcap.read()
-             count+=1
-        return frames
 
-    def get_arbitrary_images(self, get_img = False):
-        """
-        ITERATOR
-        --------
-        Purpose: 
-            Iterate over all cameras and return the link to an arbitrary images in each folder
-        
-        input: 
-            get_img: if (True), retruns a PIL image object, if False, returns links to an image -> call self.get_image(url) to get the image
-            True is not recomended as it is very slow
-        
-        yeild: 
-            folder_name: the name of the folder you are currently pulling an image from 
-            images: the arbitrary image url or pill object
-            time: the date and time as a string
-        """
-        for folder in self.folders:
-            folder_name = folder["href"]
-
-            self.imgs = self.get_images(folder_name)
-            self.dtms = self.get_datetimes(folder_name)
-
-            print(len(self.imgs), len(self.dtms))
-            if len(self.imgs) > 0:
-                self.random_select = np.random.randint(0, len(self.imgs))
-                link = f"{self.link}{folder_name}{self.imgs[self.random_select]['href']}"
-                time = self.dtms[self.random_select].text#dt.datetime.strptime(str(dtms[imgselect].text), '%y-%m-%d %H:%M  ') 
-                if get_img:
-                    yield folder_name, self.get_image(link), time
+        while cap.isOpened():
+            success, frame = cap.read()
+            if success:
+                if filepath == None:
+                    cv2.imwrite("frame%d.png" % count, frame)
                 else:
-                    yield folder_name, link, time
+                    cv2.imwrite(f"{filepath}/frame{count}.png", frame)
+                count += iter_space
+                cap.set(1, count)
             else:
-                self.random_select = None
-                yield f"{folder_name} -> empty", None, None
+                cap.release()
+        return filepath
 
-    def get_n_arbitrary_images(self, num_rand = 2):
-        """
-        ITERATOR
-        --------
-        Purpose: 
-            Iterate over all cameras and return the link to an arbitrary images in each folder
-        
-        input: 
-            get_img: if (True), retruns a PIL image object, if False, returns links to an image -> call self.get_image(url) to get the image
-            True is not recomended as it is very slow
-        
-        yeild: 
-            folder_name: the name of the folder you are currently pulling an image from 
-            images: the arbitrary image url or pill object
-            time: the date and time as a string
-        """
-        for folder in self.folders:
-            folder_name = folder["href"]
-
-            self.imgs = self.get_images(folder_name)
-            self.dtms = self.get_datetimes(folder_name)
-
-            print(len(self.imgs), len(self.dtms))
-            if num_rand > len(self.imgs):
-                links = []
-                for image in self.imgs:
-                    links.append(f"{self.link}{folder_name}{image['href']}")
-                yield folder_name, links, self.dtms
-            elif len(self.imgs) > 0:
-                #self.random_select = np.random.randint(0, len(self.imgs), num_rand)
-                self.random_select = np.sort(np.random.choice(len(self.imgs), num_rand, replace=False))
-                #print(self.random_select)
-                links = []
-                times = []
-                for index in self.random_select:
-                    links.append(f"{self.link}{folder_name}{self.imgs[index]['href']}")
-                    times.append(self.dtms[index].text)
-                yield folder_name, links, times
-            else:
-                self.random_select = None
-                yield f"{folder_name} -> empty", None, None
-    
     def get_link(self, folder, image):
         return f"{self.link}{folder}/{image}"
 
@@ -261,41 +191,6 @@ class database_video_iterator():
             for i in range(starting, len(self.imgs)):
                 yield self.get_link(folder_name, self.imgs[i].text), self.dtms[i]
     
-    def get_all_images(self, get_img = False):
-        """
-        ITERATOR
-        --------
-        Purpose: 
-            Iterate over all cameras and return the link to all images in each folder
-        
-        input: 
-            get_img: if (True), retruns a list of PIL image object, if False, returns links to each image -> call self.get_image(url) to get the image
-            True is not recomended as it is very slow
-        
-        yeild: 
-            folder_name: the name of the folder you are currently pulling images from 
-            images: the list of images urls or pill objects
-            self.dtms: the time paralleling each image in the images as a beautiful soup object, call object.text to get the date and time as a string
-        """
-        for folder in self.folders:
-            folder_name = folder["href"]
-
-            self.imgs = self.get_images(folder_name)
-            self.dtms = self.get_datetimes(folder_name)
-
-            print(len(self.imgs), len(self.dtms))
-            if get_img:
-                images = []
-                for image in self.imgs:
-                    link = f"{self.link}{folder_name}{image['href']}"
-                    images.append(self.get_image(link))
-                yield folder_name, images, self.dtms
-            else:
-                links = []
-                for image in self.imgs:
-                    links.append(f"{self.link}{folder_name}{image['href']}")
-                yield folder_name, links, self.dtms
-
 
 
 # from google.colab.patches import cv2_imshow
@@ -331,19 +226,15 @@ if __name__ == "__main__":
     for folder_name in folder_names:
         os.mkdir(f"videos/{folder_name}")
         for cam, time in k.iterate_folder(folder_name = folder_name):
-            #try:
-            if cam != None:
-                image = k.get_all_frames(cam)
-                if image != None:
+            try:
+                if cam != None:
                     fname = cam.split("/")[-1].split(".")[0]
-                    os.mkdir(f"videos/{folder_name}/{fname}")
-                    print(len(image))
-                    for i in range(0, len(image), 10):
-                        plt.imsave(f"videos/{folder_name}/{fname}/image_{i}.png", image[i])
-            # except:
-            #     print(f"{time.text} skipped")
+                    path = f"videos/{folder_name}/{fname}"
+                    os.mkdir(path)
+                    image = k.get_all_frames(cam, filepath = f"videos/{folder_name}/{fname}", iter_space = 30)
+            except KeyboardInterrupt:
+                raise
+            except:
+                print(f"{time.text} skipped")
             
 
-        os.system(f"zip {folder_name}.zip {folder_name}/*")
-        os.system(f"rm -r {folder_name}/*")
-        os.system(f"rmdir {folder_name}")
